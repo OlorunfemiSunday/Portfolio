4f035540d49c95b1ec3117dbdb3f989a4196b46d
unknown <femiglorious12@gmail.com>
2024-09-13 16:41:57 +0100

    today commit

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
   const hamburger = document.querySelector(".hamburger");
   const navItems = document.querySelector(".nav-items");
-  const socialIcons = document.querySelector(".social-icons");
 
   hamburger.addEventListener("click", function () {
     navItems.classList.toggle("active");
-    socialIcons.classList.toggle("active");
   });
 });
